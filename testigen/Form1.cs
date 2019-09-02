c2ba9e1603dc5e248cedbfe5b5997cb768622249
Robert Olsen <robert.olsen@halmstad.se>
2019-09-02 14:01:36 +0200

    Lagt till en knapp

--- testigen/Form1.cs
+++ testigen/Form1.cs
@@ -16,5 +16,10 @@ namespace testigen
         {
             InitializeComponent();
         }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello");
+        }
     }
 }
